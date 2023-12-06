8983373cd64437ae4425d1c3c6ea9063c91bcdfb
anfishka <97487005+anfishka@users.noreply.github.com>
2023-12-06 23:56:37 +0200

    Update Product.cs

--- Product.cs
+++ Product.cs
@@ -4,6 +4,8 @@ bool validInputSize = false;
 int parsedSize = 0;
 
 string [] colors = { "Red", "Green", "Yellow", "Magenta", "Blue", "White" };
+ConsoleColor[] consoleColors = { ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Magenta, ConsoleColor.Blue, ConsoleColor.White };
+
 bool validColor = false;
 
 Console.WriteLine("Enrer size of square (from 3 to 20)");
@@ -20,8 +22,26 @@ while (!validInputSize)
 
         Console.WriteLine("Enter name of color of the square (\"Red\", \"Green\", \"Yellow\", \"Magenta\", \"Blue\", \"White\")");
         
-        inputColor = Console.ReadLine();
-      //  ConsoleColor chosenColor = inputColor;
+
+        while(!validColor)
+        {
+            inputColor = Console.ReadLine();
+            for (int i = 0; i < colors.Length; i++)
+            {
+                if (string.Equals(colors[i], inputColor, StringComparison.OrdinalIgnoreCase))
+                {
+                    validColor = true;
+                    Console.BackgroundColor = consoleColors[i];
+                    break;
+                }
+            }
+            if (!validColor)
+            {
+                Console.WriteLine("Invalid color! Please enter correct name of color");
+            }
+        }
+
+        ConsoleColor defaultColor = Console.BackgroundColor;
 
 
         for (int i = 0; i < parsedSize; i++)
@@ -35,7 +55,7 @@ while (!validInputSize)
                 }
                 else
                 {
-                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.BackgroundColor = defaultColor;
                     Console.Write("   ");
                 }
 
